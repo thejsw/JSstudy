a0f968528e5bfe6235a60b8d6bab1414a7ce6b56
Sungwon Jo <jswtncapetnc@gmail.com>
2022-04-12 19:55:35 +0900

    JSday1 operator

---
+++ week1_JSbasic/day1/operator.js
@@ -0,0 +1,37 @@
+// Operator (연산자)
+
+// 산술 연산자
+console.log( 3+3 ); // 덧셈
+console.log( 3-3 ); // 뺄셈
+console.log( 3*3 ); // 곱셈
+console.log( 3/3 ); // 나눗셈
+console.log( 3%3 ); // 나머지
+console.log( 3**3 ); // 거듭제곱
+
+console.log((1+1) * 2); // 우선순위 적용
+
+console.log( 'a' + 'b' + 'c' + 'd' + 'e' + 'f' + 'u' ) // 문자열에도 적용가능 
+
+// 증감 연산자
+var num = 0;
+
+num++; // 하나씩 증가
+console.log(num);
+
+num--; // 하나씩 감소
+console.log(num);
+
+// 대입 연산자
+var num = 10; // 오른쪽 값을 왼쪽에 대입한다.
+console.log(num);
+
+console.log( num += 1 );
+console.log( num -= 1 );
+console.log( num *= 2 );
+console.log( num /= 2 );
+console.log( num %= 2 );
+
+
+// 비교 연산자
+// 할당 연산자
+// 논리 연산자
--- week1_JSbasic/day1/type.js
+++ week1_JSbasic/day1/type.js
@@ -1,5 +1,3 @@
-// this is main.js
-
 // 출력
 // // 사용 용도 : 스크립트의 데이터 값을 확인하고 싶을 때, 콘솔 패널에 값을 표시하고 싶을 때
 console.log('Hello World'); //console.log
@@ -38,43 +36,3 @@ console.log(typeof num);
 
 var str = '7'
 console.log((num+str), typeof (num+str)); // 숫자와 문자를 더하면 문자가 된다.
-
-
-
-// Operator (연산자)
-
-// 산술 연산자
-console.log( 3+3 ); // 덧셈
-console.log( 3-3 ); // 뺄셈
-console.log( 3*3 ); // 곱셈
-console.log( 3/3 ); // 나눗셈
-console.log( 3%3 ); // 나머지
-console.log( 3**3 ); // 거듭제곱
-
-console.log((1+1) * 2); // 우선순위 적용
-
-console.log( 'a' + 'b' + 'c' + 'd' + 'e' + 'f' + 'u' ) // 문자열에도 적용가능 
-
-// 증감 연산자
-var num = 0;
-
-num++; // 하나씩 증가
-console.log(num);
-
-num--; // 하나씩 감소
-console.log(num);
-
-// 대입 연산자
-var num = 10; // 오른쪽 값을 왼쪽에 대입한다.
-console.log(num);
-
-console.log( num += 1 );
-console.log( num -= 1 );
-console.log( num *= 2 );
-console.log( num /= 2 );
-console.log( num %= 2 );
-
-
-// 비교 연산자
-// 할당 연산자
-// 논리 연산자
