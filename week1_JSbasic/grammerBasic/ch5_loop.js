1981e60d158607c372938788624ed450972af930
Sungwon Jo <jswtncapetnc@gmail.com>
2022-04-13 21:38:21 +0900

    JS grammer callback

--- week1_JSbasic/grammerBasic/ch5_loop.js
+++ week1_JSbasic/grammerBasic/ch5_loop.js
@@ -13,3 +13,30 @@ for (var i=0; i<5; i++) {
     num += i
 }
 console.log(num);
+
+
+// array.forEach(function(item, index, array))
+// 콜백 개념 활용 > array의 길이만큼 callback 함수가 반복 실행된다.
+var fruits = ['apple', 'orange', 'banana'];
+var result = [];
+var listindex = [];
+
+fruits.forEach(function (item, index, array) {
+    result.push(item.toUpperCase());
+    listindex.push(index);
+})
+
+console.log(result);
+console.log(listindex);
+
+
+// var newArray = array.filter(function (item))
+// 콜백 개념 활용
+var list = [1,2,3,4,5];
+var newArray = list.filter(function (item) {
+    if (item > 3) {
+        return item;
+    }
+})
+
+console.log(newArray);
