27be17b673d34d3c3c36c48ac75b69110db3bc25
Sungwon Jo <jswtncapetnc@gmail.com>
2022-04-13 19:39:16 +0900

    JSday1 array

--- week1_JSbasic/day1/ch7_array.js
+++ week1_JSbasic/day1/ch7_array.js
@@ -37,3 +37,34 @@ for (var i=0; i<userList.length; i++) {
 }
 
 // 템플릿 리터럴 : 백틱(``) 사이에 구성된 문자열에 ${}기호를 통하여 변수나 연산을 삽입할 수 있다.
+
+
+
+// 배열 메소드
+
+// 배열.length (길이)
+var array = [1,2,3,4,5];
+console.log(array.length);
+
+// 배열.join (문자열변환)
+var array = [1,2,3,4,5];
+console.log(array.join(','));
+
+// 배열.push (추가)
+var array = [1,2,3,4,5];
+array.push(6);
+console.log(array);
+
+// 배열.pop (삭제)
+var array = [1,2,3,4,5];
+array.pop();
+console.log(array);
+
+// 배열.splice (시작점, 지울 개수, 넣을 것)
+var array = [1,2,3,4,5];
+array.splice(0,0,0);
+console.log(array);
+
+// 배열.indexOf (찾기)
+var array = [1,2,3,4,5];
+console.log(array.indexOf(3));
