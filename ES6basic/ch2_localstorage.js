24f89f1a3067e839de759f2930364bda1396ff6c
Sungwon Jo <jswtncapetnc@gmail.com>
2022-04-27 19:32:24 +0900

    ES6basic - localstorage.getItem 을 통해서 데이터 취득하기

--- ES6basic/ch2_localstorage.js
+++ ES6basic/ch2_localstorage.js
@@ -4,13 +4,27 @@ let num = 0;
 
 count.innerHTML = num;
 
+// key의 localStorage를 가져옴 (데이터 취득하기)
+if (localStorage.getItem('count') === null) {
+    num = 0;
+} else {
+    num = localStorage.getItem('count');
+}
+
 btn.addEventListener('click', () => {
     num++;
 
+    // 새로운 localstorage 생성 (데이터 추가하기)
     localStorage.setItem('count', num);
 
     count.innerHTML = num;
 })
+
+
+
+
+
+
 
 // //자바스크립트를 사용하면서 값을 저장해야 하는 경우 변수를 사용합니다.
 // 화면 이동이 있거나 영구적으로 저장해야 하는 경우 DB에 저장을 하거나 임시적으로 저장하고 싶은 경우 쿠키(cookie)를 사용하기도 합니다.
