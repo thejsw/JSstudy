eefe52e3f210b0d08161e8606300b62221a0a2c9
Sungwon Jo <jswtncapetnc@gmail.com>
2022-04-27 19:19:39 +0900

    ES6basic - ES5에서 ES6로 달라진 주요한 문법 기초 정의

---
+++ ES6basic/ch1_basic.js
@@ -0,0 +1,19 @@
+// 변수
+let 변수렛 = 0;
+const 변수콘스트 = 0;
+
+// 화살표 함수 arrow function
+
+// 구조화 destructing
+
+// spread operator
+
+// tarnary
+
+// promise
+
+
+// 데이터 저장 및 전송 : cookie, localStorage, json
+// 로그인, 장바구니
+
+
