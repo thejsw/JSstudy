6ef203c4b4ea84d134973fc5585a1d41e513f85d
Sungwon Jo <jswtncapetnc@gmail.com>
2022-05-03 19:48:04 +0900

    Node.js - fs.appendfile > 블로그 글 작성 시 새로운 파일을 생성해서 저장

--- Node.js/ssr/routes/index.js
+++ Node.js/ssr/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 // 라우팅 : 요청이 들어온 주소에 맞는 페이지를 렌더링한다.
@@ -13,6 +14,18 @@ router.get('/about', function(req, res, next) {
 
 router.get('/create', function(req, res, next) {
   res.render('blog_form', { title: 'Blog form' });
+});
+
+// 블로그 글을 post 방식으로 전달
+// 새로운 파일을 만듦
+// fs.appendFile(파일이름, 파일내용, callback)
+router.post('/create', function(req, res, next) {
+  fs.appendFile(`data/${req.body.title}.txt`, req.body.content, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/');
+  })
 });
 
 module.exports = router;
