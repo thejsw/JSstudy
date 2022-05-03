02a59c3aef15333e72243acfcb07e9226178e77f
Sungwon Jo <jswtncapetnc@gmail.com>
2022-05-03 19:35:32 +0900

    Node.js - 페이지 렌더링을 위한 라우팅 설정

--- Node.js/ssr/routes/index.js
+++ Node.js/ssr/routes/index.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// 라우팅 : 요청이 들어온 주소에 맞는 페이지를 렌더링한다.
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Blog' });
 });
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('blog_form', { title: 'Blog form' });
 });
 
 module.exports = router;
