472f8307dfbbe45fa61f02fb45450e5fdfb8e719
Sungwon Jo <jswtncapetnc@gmail.com>
2022-05-03 21:06:14 +0900

    Node.js - CRUD 게시판 구현

--- Node.js/ssr/routes/index.js
+++ Node.js/ssr/routes/index.js
@@ -4,14 +4,15 @@ var fs = require('fs');
 
 /* GET home page. */
 // 라우팅 : 요청이 들어온 주소에 맞는 페이지를 렌더링한다.
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Blog' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Blog' });
+// });
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
 });
 
+// Create
 router.get('/create', function(req, res, next) {
   res.render('blog_form', { title: 'Blog form' });
 });
@@ -26,7 +27,41 @@ router.post('/create', function(req, res, next) {
       return console.error(err);
     }
     res.redirect('/');
+  }) 
+});
+
+
+
+// Read : 블로그 글 렌더링
+router.get('/', function(req, res, next) {
+  fs.readdir('data/', (err, posts) => {
+    res.render('index', { title: 'Blog', posts: posts })
   })
 });
 
+router.get('/p/:postId', function(req, res, next) {
+  fs.readFile(`./data/${req.params.postId}`, (err, content) => {
+    if (err) {
+      return console.error(err)
+    }
+    const post = { title: req.params.postId, content: content }
+    res.render('blog_detail', { title: 'Blog', post: post })
+  })
+})
+
+
+
+
+// Delete
+router.post('/p/:postId', function(req, res, next) {
+  console.log(req.params.postId)
+  // fs.unlink(삭제할파일, callback)
+  fs.unlink(`data/${req.params.postId}`, (err) => {
+    if (err) {
+      return console.error(err)
+    }
+    // 클라이언트에게 json 데이터를 전송한다
+    res.json({ message: 'success' })
+  })
+})
 module.exports = router;
