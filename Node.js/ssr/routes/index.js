c4c753b140c53a29c04e16854eef629b3dfd0be5
Sungwon Jo <jswtncapetnc@gmail.com>
2022-05-03 21:09:22 +0900

    Node.js - CRUD 주소 겹침 문제 해결

--- Node.js/ssr/routes/index.js
+++ Node.js/ssr/routes/index.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 
 /* GET home page. */
 // 라우팅 : 요청이 들어온 주소에 맞는 페이지를 렌더링한다.
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Blog' });
-// });
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'Blog' });
+});
 
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About' });
