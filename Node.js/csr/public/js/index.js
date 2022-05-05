50a124a0bc3537fb10dd0646781fbd22864654a8
thejsw <jswtncapetnc@gmail.com>
2022-05-05 20:23:09 +0900

    Node.js - csr > 서버로의 렌더링 요청을 막고, 클라이언트에서의 이벤트 반영

--- Node.js/csr/public/js/index.js
+++ Node.js/csr/public/js/index.js
@@ -1,1 +1,10 @@
-console.log('hello js');
+document.body.addEventListener('click', e => {
+    if (e.target.matches('[data-link]')) {
+        // preventDefault() : 기본값 실행 x > 서버에 페이지 렌더링 요청 x
+        e.preventDefault();
+    
+        const url = e.target.getAttribute('href')
+    
+        console.log(url)
+    }
+})
