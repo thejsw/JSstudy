a80e090e823577544d3664f0b7f35cc47c28f2c4
thejsw <jswtncapetnc@gmail.com>
2022-07-06 15:50:50 +0900

    React Velopert - EventHandling - state에 input 값 담기

--- JSstudy/React/ReactVelopert/hello-react/src/study/ch4_EventPractice.js
+++ JSstudy/React/ReactVelopert/hello-react/src/study/ch4_EventPractice.js
@@ -1,6 +1,10 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "",
+  };
+
   render() {
     return (
       <div>
@@ -9,8 +13,11 @@ class EventPractice extends Component {
           text="text"
           name="message"
           placeholder="입력해 보세요"
+          value={this.state.message}
           onChange={(e) => {
-            console.log(e.target.value);
+            this.setState({
+              message: e.target.value,
+            });
           }}
         />
       </div>
