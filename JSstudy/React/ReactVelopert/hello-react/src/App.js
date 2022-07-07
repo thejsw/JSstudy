777a6d2fd8a7420df5c6e66f8457a738af04f2a1
thejsw <jswtncapetnc@gmail.com>
2022-07-07 18:37:31 +0900

    React Velopert - LifeCycleMethod - 를 이용해서 렌더링 과정 살펴보기

--- JSstudy/React/ReactVelopert/hello-react/src/App.js
+++ JSstudy/React/ReactVelopert/hello-react/src/App.js
@@ -1,9 +1,28 @@
 import { Component } from "react";
-import IterationSample from "./study/ch6_IterationSample.js";
+import LifeCycleSample from "./study/ch7_LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
+    );
   }
 }
 
