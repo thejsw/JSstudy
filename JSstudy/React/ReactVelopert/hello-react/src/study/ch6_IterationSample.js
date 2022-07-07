8df0c5ef7b25109aed7ed2a3ae30a1a7a3fa94ec
thejsw <jswtncapetnc@gmail.com>
2022-07-07 17:09:20 +0900

    React Velopert - map - useState와 onChange / onClick을 이용한 데이터 추가 기능 구현

--- JSstudy/React/ReactVelopert/hello-react/src/study/ch6_IterationSample.js
+++ JSstudy/React/ReactVelopert/hello-react/src/study/ch6_IterationSample.js
@@ -8,12 +8,29 @@ const IterationSample = () => {
     { id: 4, text: "바람" },
   ]);
   const [inputText, setInputText] = useState("");
-  const [nextID, setNextID] = useState("");
+  const [nextID, setNextID] = useState(5);
+
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextID,
+      text: inputText,
+    });
+    setNextID(nextID + 1);
+    setNames(nextNames);
+    setInputText("");
+  };
 
   const nameList = names.map((name, index) => (
     <li key={name.id}>{name.text}</li>
   ));
-  return <ul>{nameList}</ul>;
+  return (
+    <div>
+      <input vlaue={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>;
+    </div>
+  );
 };
 
 export default IterationSample;
