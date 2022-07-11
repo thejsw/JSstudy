d59584cfbdb3e337811ea8e20743cdf061918d45
thejsw <jswtncapetnc@gmail.com>
2022-07-11 23:19:40 +0900

    React Velopert - Component Styling - 일반 CSS를 통한 컴포넌트 스타일링

--- JSstudy/React/ReactVelopert/hello-react/src/App.js
+++ JSstudy/React/ReactVelopert/hello-react/src/App.js
@@ -1,7 +1,28 @@
-import Average from "./study/ch9_Hooks";
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = () => {
-  return <Average />;
-};
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload
+          </p>
+          <a
+            className="App-link"
+            href="http://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
