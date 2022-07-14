acf8f3225613cee0190929d5a4fb4a0f34d1e576
thejsw <jswtncapetnc@gmail.com>
2022-07-14 14:56:29 +0900

    ReactVelopert - TodoApp - todos 상태 구현하기

--- JSstudy/React/ReactVelopert/todo-app/src/components/TodoListItem.js
+++ JSstudy/React/ReactVelopert/todo-app/src/components/TodoListItem.js
@@ -1,16 +1,18 @@
 import '../style/TodoListItem.scss';
+import cn from 'classnames';
 import {
   MdCheckBoxOutlineBlank,
   MdCheckBox,
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-const TodoListItem = () => {
+const TodoListItem = ({ todo }) => {
+  const { text, checked } = todo;
   return (
     <div className="TodoListItem">
-      <div className="checkbox">
-        <MdCheckBoxOutlineBlank />
-        <div className="text">할 일</div>
+      <div className={cn('checkbox', { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
       </div>
       <div className="remove">
         <MdRemoveCircleOutline />
