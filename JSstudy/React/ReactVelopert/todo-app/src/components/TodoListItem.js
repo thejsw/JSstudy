f07c5a7d91d4ed2648dc20fd43f9edcdbe24a367
thejsw <jswtncapetnc@gmail.com>
2022-07-14 20:41:57 +0900

    ReactVelopert - 컴포넌트 성능 최적화 - react-virtualized를 통한 보이지 않는 컴포넌트 처리

--- JSstudy/React/ReactVelopert/todo-app/src/components/TodoListItem.js
+++ JSstudy/React/ReactVelopert/todo-app/src/components/TodoListItem.js
@@ -7,16 +7,21 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
