9fe24daae330bbfcacd3337703127d842f9cad92
thejsw <jswtncapetnc@gmail.com>
2022-07-14 14:34:28 +0900

    ReactVelopert - TodoApp - TodoList, TodoListItem 제작

---
+++ JSstudy/React/ReactVelopert/todo-app/src/components/TodoList.js
@@ -0,0 +1,14 @@
+import TodoListItem from './TodoListItem';
+import '../style/TodoList.scss';
+
+const TodoList = () => {
+  return (
+    <div className="TodoList">
+      <TodoListItem />
+      <TodoListItem />
+      <TodoListItem />
+    </div>
+  );
+};
+
+export default TodoList;
