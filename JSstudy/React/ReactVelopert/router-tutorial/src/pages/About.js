063c1102bdaacee0f4c478b3cc191d890d7af928
thejsw <jswtncapetnc@gmail.com>
2022-07-15 16:34:43 +0900

    ReactVelopert - SPA - useSearchParams을 통한 쿼리스트링 파싱

--- JSstudy/React/ReactVelopert/router-tutorial/src/pages/About.js
+++ JSstudy/React/ReactVelopert/router-tutorial/src/pages/About.js
@@ -1,17 +1,28 @@
-import { useLocation } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 
 const About = () => {
-  const location = useLocation();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get("detail");
+  const mode = searchParams.get("mode");
+
+  const onToggleDetail = () => {
+    setSearchParams({ mode, detail: detail === "true" ? false : true });
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParams({ mode: nextMode, detail });
+  };
 
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해보는 프로젝트입니다</p>
       <br />
-      <p>
-        주소창에 http://localhost:3000/about?detail=true&mode=1 을 입력해보세요
-      </p>
-      <p>쿼리스트링: {location.search}</p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>mode + 1</button>
     </div>
   );
 };
