d8534ea69f9e5570ab62a437beeffc460713c05f
thejsw <jswtncapetnc@gmail.com>
2022-07-15 16:29:51 +0900

    ReactVelopert - SPA - 쿼리스트링을 통한 페이지 주소 정의

--- JSstudy/React/ReactVelopert/router-tutorial/src/pages/About.js
+++ JSstudy/React/ReactVelopert/router-tutorial/src/pages/About.js
@@ -1,8 +1,17 @@
+import { useLocation } from "react-router-dom";
+
 const About = () => {
+  const location = useLocation();
+
   return (
     <div>
       <h1>소개</h1>
       <p>리액트 라우터를 사용해보는 프로젝트입니다</p>
+      <br />
+      <p>
+        주소창에 http://localhost:3000/about?detail=true&mode=1 을 입력해보세요
+      </p>
+      <p>쿼리스트링: {location.search}</p>
     </div>
   );
 };
