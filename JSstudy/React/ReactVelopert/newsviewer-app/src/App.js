41e4f2f39d3dfdd1a2f66db8a963ba7271856570
thejsw <jswtncapetnc@gmail.com>
2022-07-18 17:47:27 +0900

    ReactVelopert - newsAPI - 뉴스 뷰어 UI 제작

--- JSstudy/React/ReactVelopert/newsviewer-app/src/App.js
+++ JSstudy/React/ReactVelopert/newsviewer-app/src/App.js
@@ -1,32 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
