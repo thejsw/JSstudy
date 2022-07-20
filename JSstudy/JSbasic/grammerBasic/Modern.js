62c36fdab7458ca3848cb7080bce80eae4de9513
thejsw <jswtncapetnc@gmail.com>
2022-07-20 17:43:27 +0900

    모던한 자바스크립트 - 강의 예제 코드

---
+++ JSstudy/JSbasic/grammerBasic/Modern.js
@@ -0,0 +1,40 @@
+// let isNumber = 7;
+// let isString = "00";
+// let NumPlusString = isString + isNumber;
+// console.log(NumPlusString);
+
+// let pair = [1, 2];
+// let [first, second] = pair;
+// console.log(first, second);
+
+// let arr = [1, 2, 3];
+// for (let i of arr) {
+//   console.log(i);
+// }
+
+// let harry = { name: "h.smith", age: 36 };
+// for (let i in harry) {
+//   console.log(`${i}: ${harry[i]}`);
+// }
+
+// const average = (x, y) => {tqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtqtq
+//   return (x + y) / 2;
+// };
+
+// let result = average(2, 4);
+// console.log(result);
+
+// map을 활용하여 배열에 담긴 요소를 HTML li로 만드는 예제
+// const enclose = (tag, content) => {
+//   `<${tag}>${content}</${tag}>`;
+// };
+// const listItems = items.map((i) => enclose(li, "i"));
+
+// const sayLater = (text, when) => {
+//   let task = () => {
+//     console.log(text);
+//   };
+//   setTimeout(task, when);
+// };
+
+// sayLater("Hello", 1000);
