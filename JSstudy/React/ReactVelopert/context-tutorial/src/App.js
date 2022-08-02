911fe4b0e3735592d5b8e8c14f3fa67ac275eab5
thejsw <jswtncapetnc@gmail.com>
2022-08-02 15:44:43 +0900

    ReactVelopert - ContextAPI - 색상 선택 컴포넌트 제작

--- JSstudy/React/ReactVelopert/context-tutorial/src/App.js
+++ JSstudy/React/ReactVelopert/context-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import ColorBox from "./context/colorBox";
 import { ColorProvider } from "./context/color";
+import SelectColors from "./context/SelectColors";
 
 const App = () => {
   return (
     <ColorProvider value={{ color: "red" }}>
       <div>
+        <SelectColors />
         <ColorBox />
       </div>
     </ColorProvider>
