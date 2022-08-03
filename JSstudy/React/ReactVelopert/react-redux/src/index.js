864fa57d35c85a71275f4c4a0d220db213e7e876
thejsw <jswtncapetnc@gmail.com>
2022-08-03 19:24:36 +0900

    ReactVelopert - Redux - 리덕스 적용

--- JSstudy/React/ReactVelopert/react-redux/src/index.js
+++ JSstudy/React/ReactVelopert/react-redux/src/index.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import ReactDOM from 'react-dom';
+import { configureStore } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = configureStore(rootReducer, composeWithDevTools);
+
+ReactDOM.render(
+  // 스토어를 사용할 수 있게 하는 컴포넌트
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+  document.getElementById('root'),
 );
-
-const store = createStore(rootReducer);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
