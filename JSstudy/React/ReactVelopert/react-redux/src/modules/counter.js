ad54c5c501b85b769090a063235ba0ae8db74b5b
thejsw <jswtncapetnc@gmail.com>
2022-08-04 14:00:22 +0900

    ReactVelopert - Redux - Hooks를 활용한 컨테이너 컴포넌트 / useSelector, useDispatch, useCallback

--- JSstudy/React/ReactVelopert/react-redux/src/modules/counter.js
+++ JSstudy/React/ReactVelopert/react-redux/src/modules/counter.js
@@ -1,10 +1,12 @@
+import { createAction, handleActions } from 'redux-actions';
+
 // 액션 타입 정의
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 // 액션 함수 생성
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초깃값 정의
 const initialState = {
@@ -12,19 +14,12 @@ const initialState = {
 };
 
 // 리듀서 함수 생성
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState,
+);
 
 export default counter;
