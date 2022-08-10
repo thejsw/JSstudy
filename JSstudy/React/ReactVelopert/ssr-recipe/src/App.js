6fbca3c3e33f86e6fd822169acaee1f8c205e25f
thejsw <jswtncapetnc@gmail.com>
2022-08-10 18:53:49 +0900

    ReactVelopert - SSR - UI 준비

--- JSstudy/React/ReactVelopert/ssr-recipe/src/App.js
+++ JSstudy/React/ReactVelopert/ssr-recipe/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import loadable from "@loadable/component";
+const RedPage = loadable(() => import("./pages/RedPage"));
+const BluePage = loadable(() => import("./pages/BluePage"));
+// const UsersPage = loadable(() => import("./pages/UsersPage"));
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <hr />
+      <Route path="/red" component={RedPage} />
+      <Route path="/blue" component={BluePage} />
+      {/* <Route path="/users" component={UsersPage} /> */}
     </div>
   );
-}
+};
 
 export default App;
---
+++ JSstudy/React/ReactVelopert/ssr-recipe/src/index.server.js
@@ -0,0 +1,5 @@
+import ReactDOMServer from "react-dom/server";
+
+const html = ReactDOMServer.renderToString(<div>Hello SSR!</div>);
+
+console.log(html);
