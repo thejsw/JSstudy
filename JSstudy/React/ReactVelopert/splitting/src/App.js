0921b5644462b4cfc658435c81f295f7f264bf75
thejsw <jswtncapetnc@gmail.com>
2022-08-10 18:02:36 +0900

    ReactVelopert - Code splitting - loadable 컴포넌트 활용

--- JSstudy/React/ReactVelopert/splitting/src/App.js
+++ JSstudy/React/ReactVelopert/splitting/src/App.js
@@ -1,28 +1,27 @@
-// import React, { useState } from 'react';
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-// import loadable from '@loadable/component';
-// const SplitMe = loadable(() => import('./SplitMe'), {
-//   fallback: <div>loading...</div>
-// });
+import loadable from "@loadable/component";
+const SplitMe = loadable(() => import("./SplitMe"), {
+  fallback: <div>loading...</div>,
+});
 
 function App() {
+  const [visible, setVisible] = useState(false);
   const onClick = () => {
-    import("./notify").then((result) => result.default());
+    setVisible(true);
   };
-  // const [visible, setVisible] = useState(false);
-  // const onClick = () => {
-  //   setVisible(true);
-  // };
-  // const onMouseOver = () => {
-  //   SplitMe.preload();
-  // };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>Hello React!</p>
-        {/* {visible && <SplitMe />} */}
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          Hello React!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
