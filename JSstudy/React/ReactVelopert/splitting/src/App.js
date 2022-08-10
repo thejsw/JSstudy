7e6c299a0486e4afbf188c2ec054a2a17d80feae
thejsw <jswtncapetnc@gmail.com>
2022-08-10 17:46:36 +0900

    ReactVelopert - Code splitting - notify 파일로부터 함수를 불러옴

--- JSstudy/React/ReactVelopert/splitting/src/App.js
+++ JSstudy/React/ReactVelopert/splitting/src/App.js
@@ -1,12 +1,29 @@
+// import React, { useState } from 'react';
 import logo from "./logo.svg";
 import "./App.css";
+import notify from "./notify";
+// import loadable from '@loadable/component';
+// const SplitMe = loadable(() => import('./SplitMe'), {
+//   fallback: <div>loading...</div>
+// });
 
 function App() {
+  const onClick = () => {
+    notify();
+  };
+  // const [visible, setVisible] = useState(false);
+  // const onClick = () => {
+  //   setVisible(true);
+  // };
+  // const onMouseOver = () => {
+  //   SplitMe.preload();
+  // };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello React</p>
+        <p onClick={onClick}>Hello React!</p>
+        {/* {visible && <SplitMe />} */}
       </header>
     </div>
   );
