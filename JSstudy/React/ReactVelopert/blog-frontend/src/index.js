1d30e392e95cfe8d687716aba98a23e78931907a
thejsw <jswtncapetnc@gmail.com>
2022-08-17 18:13:21 +0900

    ReactVelopert - blogReact - 리덕스 적용

--- JSstudy/React/ReactVelopert/blog-frontend/src/index.js
+++ JSstudy/React/ReactVelopert/blog-frontend/src/index.js
@@ -2,11 +2,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+const store = configureStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    ,
+  </Provider>,
   document.getElementById('root'),
 );
