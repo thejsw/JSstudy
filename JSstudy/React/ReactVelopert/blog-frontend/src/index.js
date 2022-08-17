417ba9cf293ce4c8f92d98506e03d44b12e905cc
thejsw <jswtncapetnc@gmail.com>
2022-08-17 19:15:01 +0900

    ReactVelopert - blogReact - AuthForm, AuthTemplate 컴포넌트 작성

--- JSstudy/React/ReactVelopert/blog-frontend/src/index.js
+++ JSstudy/React/ReactVelopert/blog-frontend/src/index.js
@@ -3,11 +3,11 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { configureStore } from 'redux';
+import { legacy_createStore as createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './modules';
 
-const store = configureStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
