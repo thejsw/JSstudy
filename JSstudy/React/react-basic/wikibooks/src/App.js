3f8bf4fa772caef205205429d15ad3b87616e925
thejsw <jswtncapetnc@gmail.com>
2022-08-18 19:06:08 +0900

    React-basic - state

--- JSstudy/React/react-basic/wikibooks/src/App.js
+++ JSstudy/React/react-basic/wikibooks/src/App.js
@@ -1,54 +1,29 @@
 import React, { Component } from "react";
+import TOC from "./components/TOC.js";
+import Content from "./components/Content.js";
+import Subject from "./components/Subject.js";
 import "./App.css";
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="1.html">HTML</a>
-          </li>
-          <li>
-            <a href="2.html">CSS</a>
-          </li>
-          <li>
-            <a href="3.html">JavaScript</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>HTML</h2>
-        HTML is HyperText Markup Language.
-      </article>
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wid We!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is" },
+        { id: 2, title: "CSS", desc: "CSS is" },
+        { id: 3, title: "JS", desc: "JS is" },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="World Web"></Subject>
-        <TOC></TOC>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content></Content>
       </div>
     );
