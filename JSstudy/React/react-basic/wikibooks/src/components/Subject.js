32a0241edbc0befc67394163ff34ac8c46dbde1f
thejsw <jswtncapetnc@gmail.com>
2022-08-18 21:24:17 +0900

    React-basic - onClick event with props, state

--- JSstudy/React/react-basic/wikibooks/src/components/Subject.js
+++ JSstudy/React/react-basic/wikibooks/src/components/Subject.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 class Subject extends Component {
   render() {
+    console.log("Subject render");
     return (
       <header>
         <h1>
-          <a href="/">{this.props.title}</a>
+          <a
+            href="/"
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage();
+            }.bind(this)}
+          >
+            {this.props.title}
+          </a>
         </h1>
         {this.props.sub}
       </header>
--- JSstudy/React/react-basic/wikibooks/src/components/TOC.js
+++ JSstudy/React/react-basic/wikibooks/src/components/TOC.js
@@ -2,13 +2,23 @@ import React, { Component } from "react";
 
 class TOC extends Component {
   render() {
-    let lists = [];
-    let data = this.props.data;
-    let i = 0;
+    console.log("TOC render");
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
     while (i < data.length) {
       lists.push(
         <li key={data[i].id}>
-          <a href={"/content/" + data[i].id}>{data[i].title}</a>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (e) {
+              e.preventDefault();
+              this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}
+          >
+            {data[i].title}
+          </a>
         </li>
       );
       i = i + 1;
