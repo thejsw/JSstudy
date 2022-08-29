66e8360a6d5a333b2ddc9fd435e1f92ea90ae6c3
조성원 <jswtncapetnc@gmail.com>
2022-08-29 14:47:14 +0900

    JSstudy - ES6 - async await

---
+++ JSstudy/JSbasic/ES6/ASYNC/ch3_AsyncAwait.js
@@ -0,0 +1,34 @@
+const increase = (number) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = number + 10;
+      if (result > 50) {
+        const e = new Error("ERROR"); //--
+        return reject(e); //--
+      }
+      resolve(result); //--
+    }, 1000);
+  });
+  return promise; //--
+};
+
+async function runTasks() {
+  try {
+    let result = await increase(0);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+    result = await increase(result);
+    console.log(result);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+runTasks();
