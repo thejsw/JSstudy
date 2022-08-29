8470cb7bb9c2556c7e4184c3c0a4fb650d637f3b
조성원 <jswtncapetnc@gmail.com>
2022-08-29 14:15:41 +0900

    JSstudy - ES6 - Promise

---
+++ JSstudy/JSbasic/ES6/ASYNC/ch2_Promise.js
@@ -0,0 +1,34 @@
+const increase = (number) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = number + 10;
+      if (result > 50) {
+        const e = new Error("ERROR"); //--
+        return reject(e); //--
+      }
+      resolve(result); //--
+    }, 1000);
+  });
+  return promise; //--
+};
+
+increase(0)
+  .then((number) => {
+    console.log(number);
+    return increase(number);
+  })
+  .then((number) => {
+    console.log(number);
+    return increase(number);
+  })
+  .then((number) => {
+    console.log(number);
+    return increase(number);
+  })
+  .then((number) => {
+    console.log(number);
+    return increase(number);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
