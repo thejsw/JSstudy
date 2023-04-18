10e45572a682cd037f06c3d54f97f88ff22939ec
thejsw <jswtncapetnc@gmail.com>
2023-04-18 16:13:26 +0900

    React-props-구조분해할당,defualtprops,proptypes

--- vite-project/src/MyComponent.jsx
+++ vite-project/src/MyComponent.jsx
@@ -1,10 +1,22 @@
+import PropTypes from 'prop-types';
+
 const MyComponent = (props) => {
+    const { name, children } = props
+
     return (
         <div>
-            저는 {props.name}입니다. <br />
-            저는 {props.children}입니다.
+            저는 {name}입니다. <br />
+            저는 {children}입니다.
         </div>
     )
+
+    MyComponent.defaultProps = {
+        name: '기본 이름'
+    }
+
+    MyComponent.PropTypes = {
+        name: PropTypes.string.isRequired
+    }
 }
 
 export default MyComponent
